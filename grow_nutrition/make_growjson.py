d3435837bb7b4f5cbdcd53d97e6924805f957f89
Jack Cui <popucui@gmail.com>
2017-05-16 17:10:01 +0800

    output plain text of grow_nutrition

--- grow_nutrition/make_growjson.py
+++ grow_nutrition/make_growjson.py
@@ -1,7 +1,8 @@
-#coding=utf-8
+﻿#coding=utf-8
 
-import fire,json
+import fire, json
 import logging
+import time, os
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.CRITICAL)
 
@@ -48,6 +49,8 @@ def get_growres(grow_gtpe, growjson_file, gtest_result):
     output:
         gtest_result: output result in json
     '''
+    results_list = []
+    
     with open(growjson_file) as f:
         rs_dict = json.load(f)
     with open(grow_gtpe) as f:
@@ -71,8 +74,19 @@ def get_growres(grow_gtpe, growjson_file, gtest_result):
             rs_dict[dsection]['result'] = rs_dict[dsection]['score2result']['0']
         else:
             rs_dict[dsection]['result'] = rs_dict[dsection]['score2result']['1']
+    
+    time_stamp = time.ctime().replace(' ', '_')
+    sample_barcode = os.path.basename(grow_gtpe).split('.')[0]
+    results_list.append('snp位点\t基因\t批次\t所属样本\t基因型\t检测结果\n')
+    for dsection in rs_dict:
+        for rsid in rs_dict[dsection]:
+            if isinstance(rs_dict[dsection][rsid], dict) and ('pdpid' in rs_dict[dsection][rsid] ):
+                results_list.append('{snpid}\t\t{pici}\t{sample}\t{gtype}\t{dresult}\n'.format(
+                    snpid = rsid, pici = time_stamp, sample = sample_barcode,
+                    gtype = rs_dict[dsection][rsid]['gtype'], dresult = rs_dict[dsection]['result']) )
     with open(gtest_result, 'w', encoding='utf-8') as f:
-        json.dump(rs_dict, f, ensure_ascii=False, indent=4, sort_keys=True)
+        f.writelines(results_list)
+        # json.dump(rs_dict, f, ensure_ascii=False, indent=4, sort_keys=True)
             
             
 
